50c9236f705f687fa4473b31df5b40eed54b0ba9
crisarael <crisarael@gmail.com>
2021-09-21 19:49:40 -0700

    Primeras 10 funciones de la practica 2

---
+++ Unidad 1/Practica 2/Practica 2.R
@@ -0,0 +1,37 @@
+#Funciones encontradas en R:
+
+#1. Obtener Raiz cuadrada
+sqrt(64)
+
+#2.Extraer caracteres de una cadena de texto
+texto<-substring("Cadena de texto",3,7)
+
+#3. Obtener tipo de dato
+variable<-5
+mode(variable)
+
+#4.Obtener el directorio de trabajo actual
+getwd()
+
+#5. Crea un vector con elementos identicos
+rep(1,5)
+
+#6. Secuencia de numeros reales
+seq(1,3,5)
+
+#7.Borra objetos en memoria
+rm(name)
+
+#8. Crea un marco de datos
+expand.grid(a=c(60,80), p=c(100, 300), color=c("Blanco", "Azul"))
+
+#9. Cuenta el total de caracteres incluyendo espacios
+ncaracteres <- nchar("Total de caracteres incluyento espacios")
+ncaracteres
+
+#10.Crear lista
+lista<-list("Carro","Casa","Bicicleta")
+
+
+
+
