88f3f3a4f29131c8df46932f2c71d19cda7a274d
crisarael <crisarael@gmail.com>
2021-11-12 18:36:45 -0800

    diagrama de facetas terminado

--- Unidad 2/Practica 1/Practica 1 U2.R
+++ Unidad 2/Practica 1/Practica 1 U2.R
@@ -8,13 +8,23 @@ library(ggplot2)
 
 head(stats)
 
-#1)creating a scatter plot
+#1)creating a scatter diagram
 graphic_scatter<-ggplot(stats) +
   aes(x=Mean, y=N., color=Country) +
   geom_point()
 
 #see scatter diagram
 graphic_scatter
+
+#creating a Facet Grid
+graphic_scatter<-ggplot(stats) +
+  aes(x=Mean, y=N., color=Country) +
+  geom_point() + facet_grid(Gender~.)
+
+#see Facet Grid 
+graphic_scatter
+
+
 
 
 
